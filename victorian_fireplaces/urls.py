b7165e00068becc524d2f95452fa02e2802c8a5e
Fred Gryson <fgryson@gmail.com>
2022-04-21 11:58:51 +0100

    Changed: urlpatterns reorganised

--- victorian_fireplaces/urls.py
+++ victorian_fireplaces/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('sales/', include('sales.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('sales.urls', namespace='sales')),
 ]
