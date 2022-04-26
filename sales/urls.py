4722397b983d49ef56f57e0eb6f955e54a35740a
Fred Gryson <fgryson@gmail.com>
2022-04-26 14:47:45 +0100

    Updated: Migrations

--- sales/urls.py
+++ sales/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'sales'
 urlpatterns = [
-    path('', views.TestView.as_view() , name='test')
+    path('', views.HomeView.as_view(), name='home'),
+    path('product/<slug>/', views.ItemDetailView.as_view(), name='product'),
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,14 +1,22 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import *
 
 
 class HomeView(ListView):
     """
-    Dispaly homepage with 10 item pagination
+    Display homepage with 10 item pagination
     """
     model = Item
     paginated_by = 10
     template_name = 'home.html'
+
+
+class ItemDetailView(DetailView):
+    """
+    Display detail view for single item
+    """
+    model = Item
+    template = 'product.html'
 
 
 class TestView(TemplateView):
