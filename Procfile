aa4946592b4d58741d278945ac9460d7c1233e06
Fred Gryson <fgryson@gmail.com>
2022-05-12 09:58:02 +0100

    Added: Setup for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn victorian_fireplaces.wsgi:application
--- victorian_fireplaces/settings.py
+++ victorian_fireplaces/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import dj_database_url
 import os
 from os.path import join
 from pathlib import Path
@@ -102,13 +103,16 @@ WSGI_APPLICATION = 'victorian_fireplaces.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
